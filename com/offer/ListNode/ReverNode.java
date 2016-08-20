0e87acbe329d0df3ab0e9ecfb6b3fcaab4ad8edf
nevMor <251070568@qq.com>
2016-08-20 22:35:14 +0800

    反转链表

---
+++ com/offer/ListNode/ReverNode.java
@@ -0,0 +1,45 @@
+/**
+ * 反转链表
+ * @author zzy
+ *
+ */
+public class ReverNode {
+	class Node{
+		int val;
+		Node next;
+	}
+	//非递归
+	public static Node reverseUnRecur(Node head){
+		if(head==null || head.next==null){
+			return head;
+		}
+		Node pre = null;
+		Node next = null;
+		Node cur = head;
+		while(cur !=null){
+			//记住当前节点的next
+			next = cur.next;
+			//当前节点指向前一个节点
+			cur.next = pre;
+			//让pre记住当前节点
+			pre = cur;
+			//当前节点后移
+			cur = next;
+		}
+		return pre;
+	}
+	//递归
+	public static Node reverseRecur(Node head){
+		//递归相当于压了函数栈，栈中保存当前变量的值
+		if(head==null || head.next==null){
+			return head;
+		}
+		Node next = head.next;
+		head.next = null;
+		//想象一下，压到最后一层返回的head(revNode)就是尾节点
+		Node revNode = reverseRecur(next);
+		//此时的next和head是上一个函数栈中的数据
+		next.next = head;
+		return revNode;
+	}
+}
