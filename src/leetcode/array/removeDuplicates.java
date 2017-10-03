c04697940819865e8e99789d803b9281d6917675
zhangzhengye <zhangzhengye@shouqianba.cn>
2017-10-03 20:53:24 +0800

    leetcode

--- src/leetcode/array/removeDuplicates.java
+++ src/leetcode/array/removeDuplicates.java
@@ -5,6 +5,25 @@ package leetcode.array;
  *
  * @author zhangzhengye
  * @create 2017-10-03 下午7:32
+ *
+ * Given a sorted array, remove the duplicates in place such that each element appear
+ *
+ * only once and return the new length.
+ *
+ * Do not allocate extra space for another array,
+ *
+ * you must do this in place with constant memory.
+ *
+ *
+ * For example,
+ *
+ * Given input array nums = [1,1,2],
+ *
+ * Your function should return length = 2,
+ *
+ * with the first two elements of nums being 1 and 2 respectively.
+ *
+ * It doesn't matter what you leave beyond the new length.
  */
 public class removeDuplicates {
     public int removeDuplicates(int[] nums) {
