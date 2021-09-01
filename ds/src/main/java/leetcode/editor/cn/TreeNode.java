b0d44332433d4d6dfe70394868c3fb58a09449ba
modong@pinduoduo.com <zhengye.zhang@owitho.com>
2021-09-01 23:08:52 +0800

    BinaryTreeInorderTraversal

---
+++ ds/src/main/java/leetcode/editor/cn/TreeNode.java
@@ -0,0 +1,24 @@
+package leetcode.editor.cn;
+
+/**
+ * @date 2021-09-01 22:28
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
