2c398e8ec6e832edc713190c6de079ecb2205412
modong@pinduoduo.com <zhengye.zhang@owitho.com>
2021-09-03 09:56:08 +0800

    ConvertSortedArrayToBinarySearchTree

--- ds/src/main/java/leetcode/editor/cn/SymmetricTree.java
+++ ds/src/main/java/leetcode/editor/cn/SymmetricTree.java
@@ -33,46 +33,81 @@
 
 package leetcode.editor.cn;
 
+import java.util.LinkedList;
+
 public class SymmetricTree {
     public static void main(String[] args) {
         Solution solution = new SymmetricTree().new Solution();
     }
     //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        return symmetric(root.left, root.right);
-    }
 
-    private boolean symmetric(TreeNode left, TreeNode right) {
-        if (left == null && right == null) {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public boolean isSymmetric(TreeNode root) {
+            //return dfs(root.left, root.right);
+            return symmetric(root);
+        }
+
+        //迭代版本
+        private boolean symmetric(TreeNode root) {
+            LinkedList<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            queue.add(root);
+
+            while (!queue.isEmpty()) {
+                TreeNode left = queue.removeFirst();
+                TreeNode right = queue.removeFirst();
+                if (left == null && right == null) {
+                    continue;
+                }
+                if (left == null || right == null) {
+                    return false;
+                }
+
+                if (left.val != right.val) {
+                    return false;
+                }
+
+                queue.add(left.left);
+                queue.add(right.right);
+
+                queue.add(left.right);
+                queue.add(right.left);
+            }
+
             return true;
         }
-        if (left == null || right == null) {
-            return false;
-        }
 
-        if (left.val != right.val) {
-            return false;
-        }
+        //递归版本
+        private boolean dfs(TreeNode left, TreeNode right) {
+            if (left == null && right == null) {
+                return true;
+            }
+            if (left == null || right == null) {
+                return false;
+            }
 
-        return symmetric(left.left, right.right) && symmetric(left.right, right.left);
+            if (left.val != right.val) {
+                return false;
+            }
+
+            return dfs(left.left, right.right) && dfs(left.right, right.left);
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
