b9f095d0786a5910e7030a2137894d79a5a8e7ae
modong@pinduoduo.com <zhengye.zhang@owitho.com>
2021-10-13 09:44:47 +0800

    CountCompleteTreeNodes

---
+++ ds/src/main/java/leetcode/editor/cn/CountCompleteTreeNodes.java
@@ -0,0 +1,110 @@
+//给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
+//
+// 完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层
+//为第 h 层，则该层包含 1~ 2h 个节点。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,2,3,4,5,6]
+//输出：6
+//
+//
+// 示例 2：
+//
+//
+//输入：root = []
+//输出：0
+//
+//
+// 示例 3：
+//
+//
+//输入：root = [1]
+//输出：1
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点的数目范围是[0, 5 * 104]
+// 0 <= Node.val <= 5 * 104
+// 题目数据保证输入的树是 完全二叉树
+//
+//
+//
+//
+// 进阶：遍历树来统计节点是一种时间复杂度为 O(n) 的简单解决方案。你可以设计一个更快的算法吗？
+// Related Topics 树 深度优先搜索 二分查找 二叉树
+// 👍 546 👎 0
+
+
+package leetcode.editor.cn;
+
+public class CountCompleteTreeNodes {
+    public static void main(String[] args) {
+        Solution solution = new CountCompleteTreeNodes().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int left = getDepth(root.left);
+        int right = getDepth(root.right);
+
+        if (left == right) {
+            //左子树是满二叉树
+            return (1 << left)  + countNodes(root.right);
+        } else {
+            //最后一层不满，但倒数第二层已经满了
+            return (1 << right)  + countNodes(root.left);
+        }
+
+
+
+        //return dfs(root);
+    }
+
+    private int getDepth(TreeNode root){
+        int depth = 0;
+        while (root != null) {
+            depth++;
+            root = root.left;
+        }
+        return depth;
+    }
+
+    // 时间复杂度 O(N)
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
--- ds/src/main/java/leetcode/editor/cn/ErChaShuDeZuiJinGongGongZuXianLcof.java
+++ ds/src/main/java/leetcode/editor/cn/ErChaShuDeZuiJinGongGongZuXianLcof.java
@@ -63,6 +63,7 @@ class Solution {
         if (root.val == p.val || root.val == q.val) {
             return root;
         }
+
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
 
