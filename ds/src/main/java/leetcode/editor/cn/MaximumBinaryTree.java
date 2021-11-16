8505c41bb67af6e61db2d14b9b93667ba4e56658
modong@pinduoduo.com <zhengye.zhang@owitho.com>
2021-11-16 09:25:09 +0800

    MaximumBinaryTree

--- ds/src/main/java/leetcode/editor/cn/MaximumBinaryTree.java
+++ ds/src/main/java/leetcode/editor/cn/MaximumBinaryTree.java
@@ -1,18 +1,18 @@
-//给定一个不含重复元素的整数数组 nums 。一个以此数组直接递归构建的 最大二叉树 定义如下： 
+//给定一个不含重复元素的整数数组 nums 。一个以此数组直接递归构建的 最大二叉树 定义如下：
 //
-// 
-// 二叉树的根是数组 nums 中的最大元素。 
-// 左子树是通过数组中 最大值左边部分 递归构造出的最大二叉树。 
-// 右子树是通过数组中 最大值右边部分 递归构造出的最大二叉树。 
-// 
 //
-// 返回有给定数组 nums 构建的 最大二叉树 。 
+// 二叉树的根是数组 nums 中的最大元素。
+// 左子树是通过数组中 最大值左边部分 递归构造出的最大二叉树。
+// 右子树是通过数组中 最大值右边部分 递归构造出的最大二叉树。
 //
-// 
 //
-// 示例 1： 
+// 返回有给定数组 nums 构建的 最大二叉树 。
 //
-// 
+//
+//
+// 示例 1：
+//
+//
 //输入：nums = [3,2,1,6,0,5]
 //输出：[6,3,5,null,2,0,null,null,1]
 //解释：递归调用如下所示：
@@ -25,25 +25,25 @@
 //    - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
 //        - 只有一个元素，所以子节点是一个值为 0 的节点。
 //        - 空数组，无子节点。
-// 
 //
-// 示例 2： 
 //
-// 
+// 示例 2：
+//
+//
 //输入：nums = [3,2,1]
 //输出：[3,null,2,null,1]
-// 
 //
-// 
 //
-// 提示： 
 //
-// 
-// 1 <= nums.length <= 1000 
-// 0 <= nums[i] <= 1000 
-// nums 中的所有整数 互不相同 
-// 
-// Related Topics 栈 树 数组 分治 二叉树 单调栈 
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 1000
+// 0 <= nums[i] <= 1000
+// nums 中的所有整数 互不相同
+//
+// Related Topics 栈 树 数组 分治 二叉树 单调栈
 // 👍 340 👎 0
 
 
@@ -71,7 +71,31 @@ public class MaximumBinaryTree {
  */
 class Solution {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return construct(nums, 0, nums.length);
+    }
 
+    private TreeNode construct(int[] nums, int start, int end) {
+        if (start >= end) {
+            return null;
+        }
+        int index = maxIndex(nums, start, end);
+        TreeNode root = new TreeNode(nums[index]);
+        root.left = construct(nums, start, index);
+        root.right = construct(nums, index + 1, end);
+        return root;
+    }
+
+    private int maxIndex(int[] nums, int start, int end){
+        int max = Integer.MIN_VALUE;
+        int index = start;
+        while (start < end) {
+            if (max < nums[start]) {
+                index = start;
+                max = nums[start];
+            }
+            start++;
+        }
+        return index;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
