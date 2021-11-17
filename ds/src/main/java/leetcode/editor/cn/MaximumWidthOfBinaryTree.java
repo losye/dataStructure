577ff851c87e0cec76040640dadf6113748fd553
modong@pinduoduo.com <zhengye.zhang@owitho.com>
2021-11-17 09:41:25 +0800

    MaximumWidthOfBinaryTree

--- ds/src/main/java/leetcode/editor/cn/MaximumWidthOfBinaryTree.java
+++ ds/src/main/java/leetcode/editor/cn/MaximumWidthOfBinaryTree.java
@@ -1,75 +1,78 @@
 //给定一个二叉树，编写一个函数来获取这个树的最大宽度。树的宽度是所有层中的最大宽度。这个二叉树与满二叉树（full binary tree）结构相同，但一些节
-//点为空。 
+//点为空。
 //
-// 每一层的宽度被定义为两个端点（该层最左和最右的非空节点，两端点间的null节点也计入长度）之间的长度。 
+// 每一层的宽度被定义为两个端点（该层最左和最右的非空节点，两端点间的null节点也计入长度）之间的长度。
 //
-// 示例 1: 
+// 示例 1:
 //
-// 
-//输入: 
+//
+//输入:
 //
 //           1
 //         /   \
 //        3     2
-//       / \     \  
-//      5   3     9 
+//       / \     \
+//      5   3     9
 //
 //输出: 4
 //解释: 最大值出现在树的第 3 层，宽度为 4 (5,3,null,9)。
-// 
 //
-// 示例 2: 
 //
-// 
-//输入: 
+// 示例 2:
+//
+//
+//输入:
 //
 //          1
-//         /  
-//        3    
-//       / \       
-//      5   3     
+//         /
+//        3
+//       / \
+//      5   3
 //
 //输出: 2
 //解释: 最大值出现在树的第 3 层，宽度为 2 (5,3)。
-// 
 //
-// 示例 3: 
 //
-// 
-//输入: 
+// 示例 3:
 //
-//          1
-//         / \
-//        3   2 
-//       /        
-//      5      
 //
-//输出: 2
-//解释: 最大值出现在树的第 2 层，宽度为 2 (3,2)。
-// 
-//
-// 示例 4: 
-//
-// 
-//输入: 
+//输入:
 //
 //          1
 //         / \
 //        3   2
-//       /     \  
-//      5       9 
+//       /
+//      5
+//
+//输出: 2
+//解释: 最大值出现在树的第 2 层，宽度为 2 (3,2)。
+//
+//
+// 示例 4:
+//
+//
+//输入:
+//
+//          1
+//         / \
+//        3   2
+//       /     \
+//      5       9
 //     /         \
 //    6           7
 //输出: 8
 //解释: 最大值出现在树的第 4 层，宽度为 8 (6,null,null,null,null,null,null,7)。
-// 
 //
-// 注意: 答案在32位有符号整数的表示范围内。 
-// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 
+//
+// 注意: 答案在32位有符号整数的表示范围内。
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树
 // 👍 265 👎 0
 
 
 package leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MaximumWidthOfBinaryTree {
     public static void main(String[] args) {
@@ -92,7 +95,26 @@ public class MaximumWidthOfBinaryTree {
  * }
  */
 class Solution {
+
+    Map<Integer, Integer> depthPosMap = new HashMap<>();
+
+    private int width = 0;
+
     public int widthOfBinaryTree(TreeNode root) {
+
+        dfs(root, 0, 0);
+
+        return width;
+    }
+
+    private void dfs(TreeNode node, int depth, int pos) {
+        if (node == null) {
+            return;
+        }
+        depthPosMap.putIfAbsent(depth, pos);
+        width = Math.max(width, pos - depthPosMap.get(depth) + 1);
+        dfs(node.left, depth +1, 2 * pos);
+        dfs(node.right, depth +1, 2 * pos + 1);
 
     }
 }
