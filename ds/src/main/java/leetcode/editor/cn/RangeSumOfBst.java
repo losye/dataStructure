ed3ffaffcd851e92356335dfff4f3b5aebb1c022
modong@pinduoduo.com <zhengye.zhang@owitho.com>
2021-12-03 08:53:37 +0800

    RangeSumOfBst

---
+++ ds/src/main/java/leetcode/editor/cn/RangeSumOfBst.java
@@ -0,0 +1,93 @@
+//给定二叉搜索树的根结点 root，返回值位于范围 [low, high] 之间的所有结点的值的和。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [10,5,15,3,7,null,18], low = 7, high = 15
+//输出：32
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+//输出：23
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目在范围 [1, 2 * 104] 内
+// 1 <= Node.val <= 105
+// 1 <= low <= high <= 105
+// 所有 Node.val 互不相同
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树
+// 👍 256 👎 0
+
+
+package leetcode.editor.cn;
+
+public class RangeSumOfBst {
+    public static void main(String[] args) {
+        Solution solution = new RangeSumOfBst().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+
+        int sum = 0;
+
+        public int rangeSumBST(TreeNode root, int low, int high) {
+            if (root == null) {
+                return sum;
+            }
+            dfs(root, low, high);
+
+            return sum;
+        }
+
+        private void dfs(TreeNode root, int low, int high) {
+            if (root == null) {
+                return;
+            }
+            int val = root.val;
+
+
+            if (val > low) { //剪枝
+                dfs(root.left, low, high);
+            }
+
+            if (val >= low && val <= high) {
+                sum += val;
+            }
+
+            if (val < high) {//剪枝
+                dfs(root.right, low, high);
+            }
+
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
