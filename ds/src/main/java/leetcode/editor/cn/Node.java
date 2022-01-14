b6e90b486155480c39e12c3d3c17388780e0e21b
modong@pinduoduo.com <zhengye.zhang@owitho.com>
2022-01-14 11:40:19 +0800

    treeToDoublyList

---
+++ ds/src/main/java/leetcode/editor/cn/Node.java
@@ -0,0 +1,23 @@
+package leetcode.editor.cn;
+
+/**
+ * @author modong
+ * @date 2022-01-14 11:36
+ */
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int val) {
+        this.val = val;
+    }
+
+    public Node(int val,Node left,Node right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+};
