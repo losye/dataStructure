b06290a4204cf106368df6df0772a2eeb06821e6
modong@pinduoduo.com <zhengye.zhang@owitho.com>
2022-01-18 09:55:05 +0800

    recoverTree

--- ds/src/main/java/leetcode/editor/cn/ErChaSouSuoShuDeHouXuBianLiXuLieLcof.java
+++ ds/src/main/java/leetcode/editor/cn/ErChaSouSuoShuDeHouXuBianLiXuLieLcof.java
@@ -1,33 +1,33 @@
-//输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。如果是则返回 true，否则返回 false。假设输入的数组的任意两个数字都互不相同。 
+//输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。如果是则返回 true，否则返回 false。假设输入的数组的任意两个数字都互不相同。
 //
-// 
 //
-// 参考以下这颗二叉搜索树： 
+//
+// 参考以下这颗二叉搜索树：
 //
 //      5
 //    / \
 //   2   6
 //  / \
-// 1   3 
+// 1   3
 //
-// 示例 1： 
+// 示例 1：
 //
 // 输入: [1,6,3,2,5]
-//输出: false 
+//输出: false
 //
-// 示例 2： 
+// 示例 2：
 //
 // 输入: [1,3,2,6,5]
-//输出: true 
+//输出: true
 //
-// 
 //
-// 提示： 
 //
-// 
-// 数组长度 <= 1000 
-// 
-// Related Topics 栈 树 二叉搜索树 递归 二叉树 单调栈 
+// 提示：
+//
+//
+// 数组长度 <= 1000
+//
+// Related Topics 栈 树 二叉搜索树 递归 二叉树 单调栈
 // 👍 411 👎 0
 
 
@@ -37,12 +37,41 @@ public class ErChaSouSuoShuDeHouXuBianLiXuLieLcof {
     public static void main(String[] args) {
         Solution solution = new ErChaSouSuoShuDeHouXuBianLiXuLieLcof().new Solution();
     }
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public boolean verifyPostorder(int[] postorder) {
 
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean verifyPostorder(int[] postorder) {
+
+            return verify(postorder, 0, postorder.length - 1);
+        }
+
+        private boolean verify(int[] postorder, int lo, int hi) {
+            if (lo >= hi) {
+                return true;
+            }
+
+            int root = postorder[hi];
+
+            int rightIndex = hi;
+            for (int i = lo; i < hi; i++) {
+                if (postorder[i] > root) {
+                    rightIndex = i;
+                    break;
+                }
+            }
+
+            for(int i  = rightIndex; i < hi; i++){
+                // 如果在右子树存在小于根节点的值，说明不符合条件
+                if(postorder[i] < root){
+                    return false;
+                }
+            }
+
+            return verify(postorder, lo, rightIndex - 1) && verify(postorder, rightIndex, hi-1);
+
+
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
--- ds/src/main/java/leetcode/editor/cn/RecoverBinarySearchTree.java
+++ ds/src/main/java/leetcode/editor/cn/RecoverBinarySearchTree.java
@@ -1,33 +1,33 @@
-//给你二叉搜索树的根节点 root ，该树中的两个节点被错误地交换。请在不改变其结构的情况下，恢复这棵树。 
+//给你二叉搜索树的根节点 root ，该树中的两个节点被错误地交换。请在不改变其结构的情况下，恢复这棵树。
 //
-// 进阶：使用 O(n) 空间复杂度的解法很容易实现。你能想出一个只使用常数空间的解决方案吗？ 
+// 进阶：使用 O(n) 空间复杂度的解法很容易实现。你能想出一个只使用常数空间的解决方案吗？
 //
-// 
 //
-// 示例 1： 
 //
-// 
+// 示例 1：
+//
+//
 //输入：root = [1,3,null,null,2]
 //输出：[3,1,null,null,2]
 //解释：3 不能是 1 左孩子，因为 3 > 1 。交换 1 和 3 使二叉搜索树有效。
-// 
 //
-// 示例 2： 
 //
-// 
+// 示例 2：
+//
+//
 //输入：root = [3,1,4,null,null,2]
 //输出：[2,1,4,null,null,3]
-//解释：2 不能在 3 的右子树中，因为 2 < 3 。交换 2 和 3 使二叉搜索树有效。 
+//解释：2 不能在 3 的右子树中，因为 2 < 3 。交换 2 和 3 使二叉搜索树有效。
 //
-// 
 //
-// 提示： 
 //
-// 
-// 树上节点的数目在范围 [2, 1000] 内 
-// -231 <= Node.val <= 231 - 1 
-// 
-// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 
+// 提示：
+//
+//
+// 树上节点的数目在范围 [2, 1000] 内
+// -231 <= Node.val <= 231 - 1
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树
 // 👍 552 👎 0
 
 
@@ -54,7 +54,39 @@ public class RecoverBinarySearchTree {
  * }
  */
 class Solution {
+
+    private TreeNode first, last = null;
+
+    private TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+
     public void recoverTree(TreeNode root) {
+        dfs(root);
+        swap(first, last);
+    }
+
+    private void swap(TreeNode first, TreeNode last) {
+        int temp = first.val;
+        first.val = last.val;
+        last.val = temp;
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        dfs(root.left);
+        //  3  2  1
+        //  1  3  2  4
+        if (root.val < prev.val) {
+            if (first == null) {
+                first = prev;
+            }
+            last = root;
+        }
+        prev = root;
+
+        dfs(root.right);
 
     }
 }
