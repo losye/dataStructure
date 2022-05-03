08dbf10151237262e85ab717c0a3c64192c538b6
modong@pinduoduo.com <zhengye.zhang@owitho.com>
2022-05-03 14:46:33 +0800

    combinationSum

--- ds/src/main/java/leetcode/editor/cn/FourSjJUc.java
+++ ds/src/main/java/leetcode/editor/cn/FourSjJUc.java
@@ -1,60 +1,45 @@
-//给定一个无重复元素的正整数数组 candidates 和一个正整数 target ，找出 candidates 中所有可以使数字和为目标数 target 的
-//唯一组合。
+//给定一个可能有重复数字的整数数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合
+//。
 //
-// candidates 中的数字可以无限制重复被选取。如果至少一个所选数字数量不同，则两种组合是不同的。
-//
-// 对于给定的输入，保证和为 target 的唯一组合数少于 150 个。
+// candidates 中的每个数字在每个组合中只能使用一次，解集不能包含重复的组合。
 //
 //
 //
-// 示例 1：
+// 示例 1:
 //
 //
-//输入: candidates = [2,3,6,7], target = 7
-//输出: [[7],[2,2,3]]
+//输入: candidates = [10,1,2,7,6,1,5], target = 8,
+//输出:
+//[
+//[1,1,6],
+//[1,2,5],
+//[1,7],
+//[2,6]
+//]
+//
+// 示例 2:
 //
 //
-// 示例 2：
+//输入: candidates = [2,5,2,1,2], target = 5,
+//输出:
+//[
+//[1,2,2],
+//[5]
+//]
 //
 //
-//输入: candidates = [2,3,5], target = 8
-//输出: [[2,2,2,2],[2,3,3],[3,5]]
 //
-// 示例 3：
+// 提示:
 //
 //
-//输入: candidates = [2], target = 1
-//输出: []
-//
-//
-// 示例 4：
-//
-//
-//输入: candidates = [1], target = 1
-//输出: [[1]]
-//
-//
-// 示例 5：
-//
-//
-//输入: candidates = [1], target = 2
-//输出: [[1,1]]
+// 1 <= candidates.length <= 100
+// 1 <= candidates[i] <= 50
+// 1 <= target <= 30
 //
 //
 //
 //
-// 提示：
-//
-//
-// 1 <= candidates.length <= 30
-// 1 <= candidates[i] <= 200
-// candidate 中的每个元素都是独一无二的。
-// 1 <= target <= 500
-//
-//
-//
-//
-// 注意：本题与主站 39 题相同： https://leetcode-cn.com/problems/combination-sum/
+// 注意：本题与主站 40 题相同： https://leetcode-cn.com/problems/combination-sum-ii/
 // Related Topics 数组 回溯
 // 👍 20 👎 0
 
@@ -62,12 +47,13 @@
 package leetcode.editor.cn;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Ygoe9J {
+public class FourSjJUc {
     public static void main(String[] args) {
-        Solution solution = new Ygoe9J().new Solution();
+        Solution solution = new FourSjJUc().new Solution();
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -79,38 +65,43 @@ public class Ygoe9J {
 
         private int sum = 0;
 
-        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
             if (candidates.length == 0) {
                 return res;
             }
+            Arrays.sort(candidates);
 
             backtrack(candidates, target, 0);
 
             return res;
         }
 
+        // [10,1,2,7,6,1,5]
+        // [1,1,2,5,6,7,10]
         private void backtrack(int[] candidates, int target, int index) {
             if (sum > target) {
                 return;
             }
+
             if (sum == target) {
                 res.add(new ArrayList<>(path));
                 return;
             }
 
-
             for (int i = index; i < candidates.length; i++) {
+                if (i > index && candidates[i] == candidates[i - 1]) {
+                    continue;
+                }
                 int candidate = candidates[i];
 
                 sum += candidate;
                 path.add(candidate);
 
-                backtrack(candidates, target, i);
+                backtrack(candidates, target, i + 1);
 
                 sum -= candidate;
                 path.removeLast();
             }
-
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ ds/src/main/java/leetcode/editor/cn/PermutationILcci.java
@@ -0,0 +1,41 @@
+//无重复字符串的排列组合。编写一种方法，计算某字符串的所有排列组合，字符串每个字符均不相同。 
+//
+// 示例1: 
+//
+// 
+// 输入：S = "qwe"
+// 输出：["qwe", "qew", "wqe", "weq", "ewq", "eqw"]
+// 
+//
+// 示例2: 
+//
+// 
+// 输入：S = "ab"
+// 输出：["ab", "ba"]
+// 
+//
+// 提示: 
+//
+// 
+// 字符都是英文字母。 
+// 字符串长度在[1, 9]之间。 
+// 
+// Related Topics 字符串 回溯 
+// 👍 69 👎 0
+
+
+package leetcode.editor.cn;
+
+public class PermutationILcci {
+    public static void main(String[] args) {
+        Solution solution = new PermutationILcci().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String[] permutation(String S) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
