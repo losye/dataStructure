779f7dee6ceed3e9e60d3743ed27e26f035500b1
modong@pinduoduo.com <zhengye.zhang@owitho.com>
2022-11-29 08:46:07 +0800

    code

--- ds/src/main/java/leetcode/p2022/[1]两数之和.java
+++ ds/src/main/java/leetcode/p2022/[1]两数之和.java
@@ -1,8 +1,18 @@
+import java.util.HashMap;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
+        HashMap<Integer, Integer> valIdxMap = new HashMap<>();
+        int[] res = new int[]{0, 0};
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (valIdxMap.containsKey(target - num)) {
+               return new int[]{i, valIdxMap.get(target - num)};
+            }
+            valIdxMap.put(num, i);
+        }
+        return res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ ds/src/main/java/leetcode/p2022/[438]找到字符串中所有字母异位词.java
@@ -0,0 +1,8 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
