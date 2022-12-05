3d97934015b2a8f87cb0df4b74af82c9ac30000b
modong@pinduoduo.com <zhengye.zhang@owitho.com>
2022-12-05 08:53:06 +0800

    567 checkInclusion

--- ds/src/main/java/leetcode/p2022/[567]字符串的排列.java
+++ ds/src/main/java/leetcode/p2022/[567]字符串的排列.java
@@ -3,41 +3,34 @@ import java.util.HashMap;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        // 初始化
         HashMap<Character, Integer> need = new HashMap<>();
-        HashMap<Character, Integer> window = new HashMap<>();
         for (char c : s1.toCharArray()) {
-            Integer cnt = need.getOrDefault(c, 0);
-            need.put(c, cnt + 1);
+            int val = need.getOrDefault(c, 0);
+            need.put(c, val + 1);
         }
-        char[] s2charArray = s2.toCharArray();
-        int length = s2charArray.length;
-        int left = 0, right = 0;
-        int valid = 0;
-        // 窗口右边界
-        while (right < length) {
-            // 往窗口放
-            char rv = s2charArray[right++];
+        HashMap<Character, Integer> window = new HashMap<>();
+        char[] chars = s2.toCharArray();
+        int left = 0; int right = 0; int valid = 0;
+        while (right < chars.length) {
+            char rv = chars[right++];
             if (need.containsKey(rv)) {
                 int rc = window.getOrDefault(rv, 0);
                 window.put(rv, ++rc);
-                if (rc == need.get(rv)) {
+                if (need.get(rv) == rc) {
                     valid++;
                 }
             }
-
-            // 判断窗口是否收缩
             while (right - left >= s1.length()) {
                 if (valid == need.size()) {
                     return true;
                 }
-                char lv = s2charArray[left++];
-                int lc = window.getOrDefault(lv, 0);
+                char lv = chars[left++];
                 if (need.containsKey(lv)) {
-                    window.put(lv, lc - 1);
-                    if (lc == need.get(lv)) {
+                    int lc = window.get(lv);
+                    if (need.get(lv) == lc) {
                         valid--;
                     }
+                    window.put(lv, lc - 1);
                 }
             }
         }
